b0e1729c3d0a25973e1d724fae1b84a570e99475
JosalbertoTizon <josalberto314@gmail.com>
2025-03-10 14:35:54 -0300

    Solved Valid Palindrome and fixed a file name

---
+++ Easy Problems/Valid Palindrome/solution_1/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        print(s)
+        i = 0
+        j = len(s) - 1
+        while i <= j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
